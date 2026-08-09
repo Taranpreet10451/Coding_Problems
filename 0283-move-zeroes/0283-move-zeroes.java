d30253aa9d14180f24e11339f5f2db91ffdee1cb
Taranpreet Kaur <141022139+Taranpreet10451@users.noreply.github.com>
2026-08-09 19:29:13 +0530

    Time: 5 ms (8.25%), Space: 47.7 MB (57.81%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1){
+            return;
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                list.add(nums[i]);
+            }
+        }
+        for(int i=0;i<list.size();i++)
+        {
+            nums[i]=list.get(i);
+        }
+        for(int i=list.size();i<nums.length;i++)
+        {
+            nums[i]=0;
+        }
+    }
+}
